993c5ccaf1c8c2997c497fc6cb786c7a112c0455
tylernoblett <tyler.noblett@outlook.com>
2021-02-12 09:50:02 -0500

    remove seeding from prod

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const measureRoutes = require('./measure.route');
 const seedData = require('../config/seedData');
 const db = require('../config/sequelize');
+const config = require('../config/config');
 
 const router = express.Router(); // eslint-disable-line new-cap
 
@@ -10,11 +11,15 @@ router.get('/health-check', (req, res) => res.send('OK'));
 router.use('/measures', measureRoutes);
 
 router.use('/seed', (async (req, res) => {
-  try {
-    await Promise.all(seedData.map((record) => db.Measure.create(record)));
-    return res.send('Database seeded');
-  } catch {
-    return res.send('Database has already been seeded');
+  if (config.env === 'production') {
+    return res.send('seeding only available in development');
+  } else {
+    try {
+      await Promise.all(seedData.map((record) => db.Measure.create(record)));
+      return res.send('Database seeded');
+    } catch {
+      return res.send('Database has already been seeded');
+    }
   }
 }));
 
