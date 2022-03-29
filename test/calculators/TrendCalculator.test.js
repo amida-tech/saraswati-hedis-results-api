805fc1ed0dae844b0bb0933ec6bb86d4d5810f65
Keith <keithjayjones@gmail.com>
2022-03-29 16:21:54 -0400

    Updated test for new data

--- test/calculators/TrendCalculator.test.js
+++ test/calculators/TrendCalculator.test.js
@@ -27,8 +27,8 @@ describe(' Trend Calculation test ', () => {
   });
 
   test('Check calculations', () => {
-    expect(resultArray[0].changePercent).toEqual(9);
-    expect(resultArray[2].changePercent).toEqual(34);
-    expect(resultArray[4].changePercent).toEqual(-9);
+    expect(resultArray[0].percentChange).toEqual(-30);
+    expect(resultArray[2].percentChange).toEqual(-18);
+    expect(resultArray[4].percentChange).toEqual(23);
   });
 });
