2ecf5ae2bcd17f86d17d406b330c999420ec26fc
Tim <tim@Amidas-MacBook-Pro.local>
2022-05-24 10:26:14 -0400

    finished adding logger

--- src/consumer/consumer.js
+++ src/consumer/consumer.js
@@ -1,6 +1,7 @@
 const { Kafka } = require('kafkajs');
 const config = require('../config/config');
 const paramValidation = require('../config/param-validation');
+const logger = require('../config/winston');
 
 const {
   insertMeasure, insertMeasures,
@@ -20,6 +21,7 @@ async function kafkaRunner() {
   await consumer.subscribe({ topic: config.kafkaConfig.queue, fromBeginning: false });
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
+      logger.info('Kafka message has arrived to HERA');
       const jsonObject = JSON.parse(message.value.toString());
       if (jsonObject !== undefined && Array.isArray(jsonObject)) {
         insertMeasures(jsonObject);
