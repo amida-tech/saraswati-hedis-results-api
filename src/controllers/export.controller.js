62c4ff76c06aa9df9d5867c871a27e8f2bfc3d25
Maud <maudlin.oxalis@gmail.com>
2022-07-23 23:31:44 +0000

    async and location fixed, momentJS and some other logic removed for readability, some console logs for funsies included, pls review Eric

--- src/controllers/export.controller.js
+++ src/controllers/export.controller.js
@@ -1,46 +1,51 @@
 const fs = require('fs');
 const moment = require('moment');
+const process = require('process');
+
 const { generateTestReport } = require('../exports/test-report');
 const { generateMemberReport } = require('../exports/member-report');
 const dao = require('../config/dao');
+const __root = process.cwd();
+const cl = console.log
 
 const generateTest = async () => {
   try {
     generateTestReport();
-  } catch (e) {
-    console.log(e);
+  } catch (error) {
+    return next("ERROR: Member report failed to generate within test", error);
   }
 };
 
-// const generateMemberById = async () => {
-//   console.log('ITT we work now');
-// };
+async function generateMemberById(req, res, next) {
+  cl('Preparing to generate report...')
+  let memberResults = await dao.findMembers(req.query);
+  memberResults = memberResults.sort((a, b) => new Date(b.timeStamp) - new Date(a.timeStamp));
+  const fileName = `${memberResults[0].memberId}.xlsx`;
+  const folderPath = `/reports/member/${memberResults[0].measurementType}`;
+  cl("MemberId found within memberResult array! File name and path established.")
 
-const generateMemberById = async (req, res, next) => {
   try {
-    let memberResults = await dao.findMembers(req.query);
-    memberResults = memberResults.sort((a, b) => new Date(b.timeStamp) - new Date(a.timeStamp));
-    const fileName = `${memberResults[0].memberId}.xlsx`;
-    const folderPath = `./reports/member/${memberResults[0].measurementType}`;
-    console.log(`../${__dirname}`);
-    console.log(1);
-    fs.stat(folderPath, (error, stats) => {
-      console.log(2);
-      if (error) {
-        console.log(3);
-        return error;
+    cl("Searching for:", `${__root}${folderPath}/${fileName}`)
+
+    const status = await fs.promises.stat(`${__root}${folderPath}/${fileName}`, (error) => {
+      if (error instanceof ReferenceError) {
+        console.error("ERROR: Member report has error, regenerating file...", error);
+        return false
       }
-      if (!stats || moment(stats.mtime).isSameOrAfter(moment(), 'day')) {
-        console.log(4);
-        generateMemberReport(memberResults[0], fileName);
-      }
-      console.log(5);
-      return true;
-    });
-    console.log(6);
-    return res.sendFile(`${folderPath}/${fileName}`, { root: __dirname });
-  } catch (e) {
-    return next(e);
+    })
+
+    cl('the status:', status.ctime)
+    next("MEMBER REPORT ALREADY EXISTS")
+
+  } catch (error) {
+    if (error instanceof ReferenceError) {
+      next("GENERATION ERROR:", error)
+    } else {
+      cl("Old or missing member data. Generating fresh member report...")
+      generateMemberReport(memberResults[0], fileName);
+      res.sendFile(`.${folderPath}/${fileName}`, { root: __root });
+      return next(`SUCCESS: Member report generated in location: ${folderPath}/${fileName}`)
+    }
   }
 };
 
