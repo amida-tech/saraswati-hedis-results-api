31cfac22a873ba45799da8860f9a5b22a11d7e23
Eric <eawest2@gmail.com>
2022-07-26 12:11:55 -0400

    removed logs

--- src/controllers/export.controller.js
+++ src/controllers/export.controller.js
@@ -9,7 +9,6 @@ const dao = require('../config/dao');
 
 // eslint-disable-next-line no-underscore-dangle
 const __root = process.cwd();
-const cl = console.log;
 
 const generateTest = async () => {
   try {
@@ -22,18 +21,14 @@ const generateTest = async () => {
 
 // eslint-disable-next-line consistent-return
 async function generateMemberById(req, res, next) {
-  cl('Preparing to generate report...');
   let memberResults = await dao.findMembers(req.query);
   memberResults = memberResults.sort((a, b) => new Date(b.timeStamp) - new Date(a.timeStamp));
   const fileName = `${memberResults[0].memberId}.xlsx`;
   const folderPath = `/reports/member/${memberResults[0].measurementType}`;
   const filePath = `${__root}${folderPath}/${fileName}`;
-  cl('MemberId found within memberResult array! File name and path established.');
 
   res.set('Content-Disposition', `attachment; filename="${fileName}"`);
   try {
-    cl('Searching for:', filePath);
-
     const status = await fs.promises.stat(filePath, (error) => {
       if (error instanceof ReferenceError) {
         console.error('ERROR: Member report has error, regenerating file...', error);
@@ -42,8 +37,6 @@ async function generateMemberById(req, res, next) {
       return true;
     });
     const validTime = moment().isSame(moment(status.ctime), 'day');
-    cl(validTime);
-    cl('the status:', status.ctime);
     if (validTime) {
       res.download(filePath);
     } else {
@@ -54,7 +47,6 @@ async function generateMemberById(req, res, next) {
     if (error instanceof ReferenceError) {
       next('GENERATION ERROR:', error);
     } else {
-      cl('Old or missing member data. Generating fresh member report...');
       generateMemberReport(memberResults[0], fileName);
       res.download(filePath);
       return next(`SUCCESS: Member report generated in location: ${folderPath}/${fileName}`);
