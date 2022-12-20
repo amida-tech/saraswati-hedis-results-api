88fb26b7a326fc5ec450476e0dae9eadb8af5954
Keith <keithjayjones@gmail.com>
2022-12-20 16:16:11 -0500

    Fixed qrda 3 bug

--- src/exports/qrda-3-report.js
+++ src/exports/qrda-3-report.js
@@ -81,7 +81,7 @@ const numeratorValueObject = () => ({
 });
 
 const getMeasureText = (result, measureInfo) => {
-  const resultInfo = measureInfo[result.measurementType];
+  const resultInfo = measureInfo[result.measure];
   if (resultInfo.description) {
     return `${resultInfo.title} - ${resultInfo.title}`;
   }
