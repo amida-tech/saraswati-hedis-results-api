ea723a41c717787a6449a1b6258152bca93b6a35
m-ox <maudlin.oxalis@gmail.com>
2023-02-28 15:22:44 +0000

    fixed typos

--- src/utilities/infoUtil.js
+++ src/utilities/infoUtil.js
@@ -3,9 +3,9 @@ const measureInfo = require('../../initialize/hedis-info.json');
 const measureLinks = [];
 
 measureInfo.forEach((measure) => {
-  if (!measure.measureTypes.split('').includes('-')) {
+  if (!measure.measureType.split('').includes('-')) {
     measureLinks.push(
-      { measure: measure.measureTypes, link: measure.info.link },
+      { measure: measure.measureType, link: measure.info.link },
     );
   }
 });
@@ -14,13 +14,13 @@ const createInfoObject = (infoList) => {
   const fullInfo = {};
   for (let i = 0; i < infoList.length; i += 1) {
     const info = infoList[i];
-    fullInfo[info.measureTypes] = info.info;
+    fullInfo[info.measureType] = info.info;
 
     // eslint-disable-next-line max-len
-    const foundLinkObj = measureLinks.filter((measureLink) => info.measureTypes.includes(measureLink.measure));
+    const foundLinkObj = measureLinks.filter((measureLink) => info.measureType.includes(measureLink.measure));
     if (foundLinkObj.length !== 0) {
       const { link } = foundLinkObj[0];
-      fullInfo[info.measureTypes] = { ...fullInfo[info.measureTypes], link };
+      fullInfo[info.measureType] = { ...fullInfo[info.measureType], link };
     }
   }
   return fullInfo;
