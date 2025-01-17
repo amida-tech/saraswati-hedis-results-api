1c53de9e78bc4b417f1ac083cef67fad505106be
jfetterolf <jonathan.fetterolf@amida.com>
2025-01-17 10:19:25 -0500

    fix filter route

--- src/routes/filterSearch.route.js
+++ src/routes/filterSearch.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const router = express(); // eslint-disable-line new-cap
+const router = express.Router(); // eslint-disable-line new-cap
 const { filterMembers, getDailyMeasureResults } = require('../controllers/filterSearch.controller');
 
 if (process.env.NODE_ENV !== 'production') {
