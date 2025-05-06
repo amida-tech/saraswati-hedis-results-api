860ca357e77f0c8cd94565a412a3bc8992a07f26
Keith <keithjayjones@gmail.com>
2025-05-05 22:52:01 -0400

    Fixes deployment and dev container file (#176)

--- src/utilities/infoUtil.js
+++ src/utilities/infoUtil.js
@@ -14,13 +14,15 @@ const createInfoObject = (infoList) => {
   const fullInfo = {};
   for (let i = 0; i < infoList.length; i += 1) {
     const info = infoList[i];
-    fullInfo[info.measureType] = info.info;
+    // eslint-disable-next-line no-underscore-dangle
+    const measureType = info._id;
+    fullInfo[measureType] = info[measureType];
 
     // eslint-disable-next-line max-len
-    const foundLinkObj = measureLinks.filter((measureLink) => info.measureType.includes(measureLink.measure));
-    if (foundLinkObj.length !== 0) {
-      const { link } = foundLinkObj[0];
-      fullInfo[info.measureType] = { ...fullInfo[info.measureType], link };
+    const foundLinkObj = measureLinks.filter((measureLink) => measureType === measureLink.measure);
+    if (foundLinkObj) {
+      const { link } = foundLinkObj;
+      fullInfo[measureType] = { ...fullInfo[measureType], link };
     }
   }
   return fullInfo;
